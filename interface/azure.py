9809fc95efd8263861ab5cc8bea8aa20d267360e
Nativu5 <sdtalyk@163.com>
2022-03-06 23:56:43 +0800

    :construction: Implement azure REST API

---
+++ interface/azure.py
@@ -0,0 +1,12 @@
+import requests
+
+subscription_key = 'REPLACE_WITH_YOUR_KEY'
+
+def get_token(subscription_key):
+    fetch_token_url = 'https://westus.api.cognitive.microsoft.com/sts/v1.0/issueToken'
+    headers = {
+        'Ocp-Apim-Subscription-Key': subscription_key
+    }
+    response = requests.post(fetch_token_url, headers=headers)
+    access_token = str(response.text)
+    print(access_token)

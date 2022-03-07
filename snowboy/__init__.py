a616214712a0ee923ee8604046c743bc53ddd57b
Nativu5 <sdtalyk@163.com>
2022-03-07 17:45:56 +0800

    :zap: Lower sensitivity

--- snowboy/__init__.py
+++ snowboy/__init__.py
@@ -1,4 +1,4 @@
 from . import snowboydecoder
 
-def get_detector(model, sensitivity=0.5):
+def get_detector(model, sensitivity=0.38):
     return snowboydecoder.HotwordDetector(decoder_model=model, sensitivity=sensitivity)

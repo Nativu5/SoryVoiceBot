1737f62b1e70869e16add3a945b68c60269d883d
Nativu5 <sdtalyk@163.com>
2022-03-07 17:44:06 +0800

    :sparkles: Implement Azure STT REST API

--- entities/__init__.py
+++ entities/__init__.py
@@ -1,32 +1,32 @@
-import speech_recognition as sr
 import os
 from interface.led import LED as _LED
+from interface.azure import parse_wav_file, recognize
+from utils.log import init_logging
+
+logger = init_logging(__name__)
+
 
 class Sory:
-    def __init__(self, detector, led : _LED, config) -> None:
+    def __init__(self, detector, led: _LED, config) -> None:
         self.detector = detector
-        self.LED = led
         self.config = config
+        self.LED = led
 
     def detected_callback(self, fname: str = ""):
-        # snowboydecoder.play_audio_file()
         self.LED.power.on()
         for i in range(0, 12):
-            self.LED.switch_by_place(i, color={"r": 255, "g": 0, "b": 0}, bright=30)
+            self.LED.switch_by_place(
+                i, color={"r": 255, "g": 0, "b": 0}, bright=30)
         print('Recording audio...', end='', flush=True)
 
-    def audioRecorderCallback(self, fname):
-        print("converting audio to text")
-        r = sr.Recognizer()
-        with sr.AudioFile(fname) as source:
-            audio = r.record(source)
+    def audio_recorder_callback(self, fname):
+        print("Converting audio to text")
         try:
-            print(r.recognize_bing(audio_data=audio, key=self.config.azure_key, language='zh-CN'))
-            # print(r.recognize_google(audio, language="zh-yue"))
-        except sr.UnknownValueError:
-            print("Could not understand audio")
-        except sr.RequestError as e:
-            print(
+            audio_data = parse_wav_file(fname)
+            logger.info(
+                recognize(audio_data, self.config.azure_key, "japaneast", "zh-CN"))
+        except Exception as e:
+            logger.warning(
                 "Could not request results from Speech Recognition service; {0}".format(e))
 
         os.remove(fname)
--- interface/azure.py
+++ interface/azure.py
@@ -1,12 +1,82 @@
 import requests
+from utils.log import init_logging
 
-subscription_key = 'REPLACE_WITH_YOUR_KEY'
+logger = init_logging(__name__)
 
-def get_token(subscription_key):
-    fetch_token_url = 'https://westus.api.cognitive.microsoft.com/sts/v1.0/issueToken'
+
+def get_token(region, subscription_key):
+    fetch_token_url = 'https://{}.api.cognitive.microsoft.com/sts/v1.0/issueToken'.format(
+        region)
     headers = {
         'Ocp-Apim-Subscription-Key': subscription_key
     }
-    response = requests.post(fetch_token_url, headers=headers)
-    access_token = str(response.text)
-    print(access_token)
+
+    try:
+        response = requests.post(fetch_token_url, headers=headers)
+        response.raise_for_status()
+        access_token = str(response.text)
+    except Exception as e:
+        logger.error("Cannot obtain access token from Azure: {}".format(e))
+        raise e
+
+    return access_token
+
+
+def parse_wav_file(fname):
+    try:
+        with open(fname, "rb") as fo:
+            raw = fo.read()
+    except FileNotFoundError as e:
+        logger.error("Cannot open recorded wav file: {}".format(e))
+        raise e
+    return raw
+
+
+def recognize(audio_data, key, region, language, format="simple", profanity="masked", model_cid=None, show_all=False):
+    headers = {
+        "Content-type": "audio/wav; codecs=audio/pcm; samplerate=16000",
+        "Accept": "application/json"
+    }
+
+    try:
+        access_token = get_token(region, key)
+    except Exception:
+        logger.warning("Change to use subscription_key directly.")
+        headers["Ocp-Apim-Subscription-Key"] = key
+    else:
+        headers["authorization"] = "Bearer {}".format(access_token)
+
+    recognize_url = 'https://{}.stt.speech.microsoft.com/speech/recognition/conversation/cognitiveservices/v1'.format(
+        region)
+
+    params = {
+        "language": language,
+        "format": format,
+        "profanity": profanity
+    }
+    if model_cid is not None:
+        params["cid"] = model_cid
+
+    try:
+        response = requests.post(
+            recognize_url, headers=headers, params=params, data=audio_data)
+        response.raise_for_status()
+        response.encoding = response.apparent_encoding
+        r_json = response.json()
+    except Exception as e:
+        logger.error("Cannot obtain recognition result: ".format(e))
+        raise e
+
+    try:
+        text = str(r_json["DisplayText"])
+    except:
+        raise Exception("Invalid recognition output: ".format(r_json))
+
+    return r_json if show_all else text
+
+
+if __name__ == "__main__":
+    region = "japaneast"
+    subscription_key = 'some_key'
+    audio_data = parse_wav_file("some.wav")
+    recognize(audio_data, subscription_key, region, "zh-CN")
